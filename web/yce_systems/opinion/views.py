8503fa6a0344ee4479dee4e7bae25388b1db4beb
geek hub <59388282+geekhubshow@users.noreply.github.com>
2022-01-07 14:42:58 +0300

    With Django

--- web/yce_systems/opinion/views.py
+++ web/yce_systems/opinion/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+
+def index(request):
+    return HttpResponse('<h2>Hello, world. You\'re at the page 1.</h2>')
 # Create your views here.
--- web/yce_systems/yce_systems/urls.py
+++ web/yce_systems/yce_systems/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('search.urls')),
+    path('account/', include('account.urls')),
+    path('opinion/', include('opinion.urls')),
+    path('bugreports/', include('bugreports.urls')),
+    path('agreement/', include('agreement.urls')),
 ]
